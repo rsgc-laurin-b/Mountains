7c48043ea80671228a63c577b79c083595bb3a00
Ben Laurin <benlaurin@Bens-MacBook-Pro-2.local>
2017-02-03 10:55:34 -0500

    loops done

--- Mountains/Mountains.playground/Pages/Goal.xcplaygroundpage/Contents.swift
+++ Mountains/Mountains.playground/Pages/Goal.xcplaygroundpage/Contents.swift
@@ -6,142 +6,45 @@
  */
 //: ## Template code
 //: The following two statements are required to make the playground run. Please do not remove.
-import Cocoa
-import PlaygroundSupport
+
 //: ## Reminder
 //: To see the visual output, be sure to show the Assistant Editor and choose the Timeline option.
 //:
 //: ![assistanteditor](timeline.png "Timeline")
 //: ## Your code starts here
+
+
+
+import Cocoa
+import PlaygroundSupport
+
+
 // Create a new canvas
 let canvas = Canvas(width: 300, height: 300)
 
-canvas.drawEllipse(centreX: 0, centreY: 300, width: 2, height: 2)
-canvas.drawEllipse(centreX: 25, centreY: 300, width: 2, height: 2)
-canvas.drawEllipse(centreX: 50, centreY: 300, width: 2, height: 2)
-canvas.drawEllipse(centreX: 75, centreY: 300, width: 2, height: 2)
-canvas.drawEllipse(centreX: 100, centreY: 300, width: 2, height: 2)
-canvas.drawEllipse(centreX: 125, centreY: 300, width: 2, height: 2)
-canvas.drawEllipse(centreX: 150, centreY: 300, width: 2, height: 2)
-canvas.drawEllipse(centreX: 175, centreY: 300, width: 2, height: 2)
-canvas.drawEllipse(centreX: 200, centreY: 300, width: 2, height: 2)
-canvas.drawEllipse(centreX: 225, centreY: 300, width: 2, height: 2)
-canvas.drawEllipse(centreX: 250, centreY: 300, width: 2, height: 2)
-canvas.drawEllipse(centreX: 275, centreY: 300, width: 2, height: 2)
-canvas.drawEllipse(centreX: 300, centreY: 300, width: 2, height: 2)
-// second row dots
-canvas.drawEllipse(centreX: 0, centreY: 275, width: 2, height: 2)
-canvas.drawEllipse(centreX: 25, centreY: 275, width: 2, height: 2)
-canvas.drawEllipse(centreX: 50, centreY: 275, width: 2, height: 2)
-canvas.drawEllipse(centreX: 75, centreY: 275, width: 2, height: 2)
-canvas.drawEllipse(centreX: 100, centreY: 275, width: 2, height: 2)
-canvas.drawEllipse(centreX: 125, centreY: 275, width: 2, height: 2)
-canvas.drawEllipse(centreX: 150, centreY: 275, width: 2, height: 2)
-canvas.drawEllipse(centreX: 175, centreY: 275, width: 2, height: 2)
-canvas.drawEllipse(centreX: 200, centreY: 275, width: 2, height: 2)
-canvas.drawEllipse(centreX: 225, centreY: 275, width: 2, height: 2)
-canvas.drawEllipse(centreX: 250, centreY: 275, width: 2, height: 2)
-canvas.drawEllipse(centreX: 275, centreY: 275, width: 2, height: 2)
-canvas.drawEllipse(centreX: 300, centreY: 275, width: 2, height: 2)
-// Third row dots
-canvas.drawEllipse(centreX: 0, centreY: 250, width: 2, height: 2)
-canvas.drawEllipse(centreX: 25, centreY: 250, width: 2, height: 2)
-canvas.drawEllipse(centreX: 50, centreY: 250, width: 2, height: 2)
-canvas.drawEllipse(centreX: 75, centreY: 250, width: 2, height: 2)
-canvas.drawEllipse(centreX: 100, centreY: 250, width: 2, height: 2)
-canvas.drawEllipse(centreX: 125, centreY: 250, width: 2, height: 2)
-canvas.drawEllipse(centreX: 150, centreY: 250, width: 2, height: 2)
-canvas.drawEllipse(centreX: 175, centreY: 250, width: 2, height: 2)
-canvas.drawEllipse(centreX: 200, centreY: 250, width: 2, height: 2)
-canvas.drawEllipse(centreX: 225, centreY: 250, width: 2, height: 2)
-canvas.drawEllipse(centreX: 250, centreY: 250, width: 2, height: 2)
-canvas.drawEllipse(centreX: 275, centreY: 250, width: 2, height: 2)
-canvas.drawEllipse(centreX: 300, centreY: 250, width: 2, height: 2)
-
-// Fourth row dots
-canvas.drawEllipse(centreX: 0, centreY: 225, width: 2, height: 2)
-canvas.drawEllipse(centreX: 25, centreY: 225, width: 2, height: 2)
-canvas.drawEllipse(centreX: 50, centreY: 225, width: 2, height: 2)
-canvas.drawEllipse(centreX: 75, centreY: 225, width: 2, height: 2)
-canvas.drawEllipse(centreX: 100, centreY: 225, width: 2, height: 2)
-canvas.drawEllipse(centreX: 125, centreY: 225, width: 2, height: 2)
-canvas.drawEllipse(centreX: 150, centreY: 225, width: 2, height: 2)
-canvas.drawEllipse(centreX: 175, centreY: 225, width: 2, height: 2)
-canvas.drawEllipse(centreX: 200, centreY: 225, width: 2, height: 2)
-canvas.drawEllipse(centreX: 225, centreY: 225, width: 2, height: 2)
-canvas.drawEllipse(centreX: 250, centreY: 225, width: 2, height: 2)
-canvas.drawEllipse(centreX: 275, centreY: 225, width: 2, height: 2)
-canvas.drawEllipse(centreX: 300, centreY: 225, width: 2, height: 2)
-
-//Fifth Row Dots
-canvas.drawEllipse(centreX: 0, centreY: 200, width: 2, height: 2)
-canvas.drawEllipse(centreX: 25, centreY: 200, width: 2, height: 2)
-canvas.drawEllipse(centreX: 50, centreY: 200, width: 2, height: 2)
-canvas.drawEllipse(centreX: 75, centreY: 200, width: 2, height: 2)
-canvas.drawEllipse(centreX: 100, centreY: 200, width: 2, height: 2)
-canvas.drawEllipse(centreX: 125, centreY: 200, width: 2, height: 2)
-canvas.drawEllipse(centreX: 150, centreY: 200, width: 2, height: 2)
-canvas.drawEllipse(centreX: 175, centreY: 200, width: 2, height: 2)
-canvas.drawEllipse(centreX: 200, centreY: 200, width: 2, height: 2)
-canvas.drawEllipse(centreX: 225, centreY: 200, width: 2, height: 2)
-canvas.drawEllipse(centreX: 250, centreY: 200, width: 2, height: 2)
-canvas.drawEllipse(centreX: 275, centreY: 200, width: 2, height: 2)
-canvas.drawEllipse(centreX: 300, centreY: 200, width: 2, height: 2)
-
-// Sixth Row Dots
-canvas.drawEllipse(centreX: 0, centreY: 175, width: 2, height: 2)
-canvas.drawEllipse(centreX: 25, centreY: 175, width: 2, height: 2)
-canvas.drawEllipse(centreX: 50, centreY: 175, width: 2, height: 2)
-canvas.drawEllipse(centreX: 75, centreY: 175, width: 2, height: 2)
-canvas.drawEllipse(centreX: 100, centreY: 175, width: 2, height: 2)
-canvas.drawEllipse(centreX: 125, centreY: 175, width: 2, height: 2)
-canvas.drawEllipse(centreX: 150, centreY: 175, width: 2, height: 2)
-canvas.drawEllipse(centreX: 175, centreY: 175, width: 2, height: 2)
-canvas.drawEllipse(centreX: 200, centreY: 175, width: 2, height: 2)
-canvas.drawEllipse(centreX: 225, centreY: 175, width: 2, height: 2)
-canvas.drawEllipse(centreX: 250, centreY: 175, width: 2, height: 2)
-canvas.drawEllipse(centreX: 275, centreY: 175, width: 2, height: 2)
-canvas.drawEllipse(centreX: 300, centreY: 175, width: 2, height: 2)
-
-// Seventh row of dots
-canvas.drawEllipse(centreX: 0, centreY: 150, width: 2, height: 2)
-canvas.drawEllipse(centreX: 25, centreY: 150, width: 2, height: 2)
-canvas.drawEllipse(centreX: 50, centreY: 150, width: 2, height: 2)
-canvas.drawEllipse(centreX: 75, centreY: 150, width: 2, height: 2)
-canvas.drawEllipse(centreX: 100, centreY: 150, width: 2, height: 2)
-canvas.drawEllipse(centreX: 125, centreY: 150, width: 2, height: 2)
-canvas.drawEllipse(centreX: 150, centreY: 150, width: 2, height: 2)
-canvas.drawEllipse(centreX: 175, centreY: 150, width: 2, height: 2)
-canvas.drawEllipse(centreX: 200, centreY: 150, width: 2, height: 2)
-canvas.drawEllipse(centreX: 225, centreY: 150, width: 2, height: 2)
-canvas.drawEllipse(centreX: 250, centreY: 150, width: 2, height: 2)
-canvas.drawEllipse(centreX: 275, centreY: 150, width: 2, height: 2)
-canvas.drawEllipse(centreX: 300, centreY: 150, width: 2, height: 2)
-
-//Eights row of dots
-canvas.drawEllipse(centreX: 0, centreY: 125, width: 2, height: 2)
-canvas.drawEllipse(centreX: 25, centreY: 125, width: 2, height: 2)
-canvas.drawEllipse(centreX: 50, centreY: 125, width: 2, height: 2)
-canvas.drawEllipse(centreX: 75, centreY: 125, width: 2, height: 2)
-canvas.drawEllipse(centreX: 100, centreY: 125, width: 2, height: 2)
-canvas.drawEllipse(centreX: 125, centreY: 125, width: 2, height: 2)
-canvas.drawEllipse(centreX: 150, centreY: 125, width: 2, height: 2)
-canvas.drawEllipse(centreX: 175, centreY: 125, width: 2, height: 2)
-canvas.drawEllipse(centreX: 200, centreY: 125, width: 2, height: 2)
-canvas.drawEllipse(centreX: 225, centreY: 125, width: 2, height: 2)
-canvas.drawEllipse(centreX: 250, centreY: 125, width: 2, height: 2)
-canvas.drawEllipse(centreX: 275, centreY: 125, width: 2, height: 2)
-canvas.drawEllipse(centreX: 300, centreY: 125, width: 2, height: 2)
+for x in stride(from: 0, through: 300, by: 25){
+    canvas.drawEllipse(centreX: x, centreY: 300, width: 2, height: 2)
+    canvas.drawEllipse(centreX: x, centreY: 275, width: 2, height: 2)
+    canvas.drawEllipse(centreX: x, centreY: 250, width: 2, height: 2)
+    canvas.drawEllipse(centreX: x, centreY: 225, width: 2, height: 2)
+    canvas.drawEllipse(centreX: x, centreY: 200, width: 2, height: 2)
+    canvas.drawEllipse(centreX: x, centreY: 175, width: 2, height: 2)
+    canvas.drawEllipse(centreX: x, centreY: 150, width: 2, height: 2)
+    canvas.drawEllipse(centreX: x, centreY: 125, width: 2, height: 2)
+    canvas.drawEllipse(centreX: x, centreY: 100, width: 2, height: 2)
+    canvas.drawEllipse(centreX: x, centreY: 75, width: 2, height: 2)
+    canvas.drawEllipse(centreX: x, centreY: 50, width: 2, height: 2)
+    canvas.drawEllipse(centreX: x, centreY: 25, width: 2, height: 2)
+}
 
 // Big Mountain Rectangle
 
 canvas.fillColor = Color(hue: 0, saturation: 000, brightness: 70, alpha: 100)
 canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 300, height: 150)
 
-canvas.fillcolor =
 canvas.fillColor = Color(hue: 0, saturation: 000, brightness: 70, alpha: 100)
 canvas.drawLine(fromX: 0, fromY: 175, toX: 25, toY: 150, lineWidth: 35)
-
+canvas.drawLine(fromX: 0, fromY: 175, toX: 25, toY: 150, lineWidth: 35)
 //: ## Template code
 //: The code below is necessary to see the result of your work in the Assistant Editor at right. Please do not remove.
 PlaygroundPage.current.liveView = canvas.imageView
